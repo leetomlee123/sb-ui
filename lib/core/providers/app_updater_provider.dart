ae425d4cb3def5340bddd6d11419e991f3bd0cb5
leetomlee123 <leetomlee123@gmail.com>
2026-08-26 06:14:27 +0000

    chore(release): bump version to v1.2.19+39

--- lib/core/providers/app_updater_provider.dart
+++ lib/core/providers/app_updater_provider.dart
@@ -134,7 +134,7 @@ class AppUpdaterNotifier extends StateNotifier<AppUpdaterState> {
         return normalizeSemver(info.version);
       }
     } catch (_) {}
-    return '1.2.18';
+    return '1.2.19';
   }
 
   void _syncProxy() {
